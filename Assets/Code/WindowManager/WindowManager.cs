302806a86afb61468c6118c6d69236d19a8fb00d
Quonce <quonce@mail.ru>
2019-02-12 22:22:44 +0300

    Add example of using code in Unity project

---
+++ Assets/Code/WindowManager/WindowManager.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace WindowManager
+{
+    public class Window : MonoBehaviour
+    {
+        private static Dictionary<string, Animator> inactiveWindows = new Dictionary<string, Animator>();
+        private static Dictionary<string, Animator> activeWindows = new Dictionary<string, Animator>();
+
+        private static bool canCloseLast;
+
+        public static void Init(bool AtLeastOneWindowAlwaysActive)
+        {
+            canCloseLast = !AtLeastOneWindowAlwaysActive;
+            Animator[] windows = FindObjectsOfType<Animator>();
+
+            inactiveWindows.Clear();
+            for (int i = 0; i < windows.Length; i++)
+            {
+                if (inactiveWindows.ContainsKey(windows[i].name))
+                {
+                    Debug.LogError($"Window with name {windows[i].name} already exists!! Please rename one of this windows.");
+                    return;
+                }
+                inactiveWindows.Add(windows[i].name, windows[i]);
+            }
+        }
+
+        public static void Open(string name)
+        {
+            if (inactiveWindows.ContainsKey(name))
+            {
+                if (activeWindows.ContainsKey(name))
+                {
+                    Debug.LogError($"Window {name} appears in both Dictionarys!! WTF??? CHECK YOUR CODE!!! Do not add windows manually!");
+                }
+                else
+                {
+                    activeWindows.Add(name, inactiveWindows[name]);
+                    inactiveWindows.Remove(name);
+
+                    activeWindows[name].SetTrigger("Open");
+                }
+            }
+            else
+            {
+                if (!activeWindows.ContainsKey(name))
+                    Debug.LogError($"There is no window with name {name}!! Please check name of window you call.");
+                else
+                    Debug.Log($"Window {name} is already active!");
+            }
+        }
+
+        public static void Close(string name)
+        {
+            if (activeWindows.ContainsKey(name))
+            {
+                if (inactiveWindows.ContainsKey(name))
+                {
+                    Debug.LogError($"Window {name} appears in both Dictionarys!! WTF??? CHECK YOUR CODE!!! Do not add windows manually!");
+                }
+                else
+                {
+                    inactiveWindows.Add(name, activeWindows[name]);
+                    activeWindows.Remove(name);
+
+                    inactiveWindows[name].SetTrigger("Close");
+                }
+            }
+            else
+            {
+                if (!inactiveWindows.ContainsKey(name))
+                    Debug.LogError($"There is no window with name {name}!! Please check name of window you call.");
+                else
+                    Debug.Log($"Window {name} is already inactive!");
+            }
+        }
+
+        public static bool isActive(string name)
+        {
+            if (activeWindows.ContainsKey(name))
+            {
+                return true;
+            }
+            else
+            {
+                if (inactiveWindows.ContainsKey(name))
+                    return false;
+                else
+                {
+                    Debug.LogError($"There is no window with name {name}!! Please check name of window you call.");
+                    return false;
+                }
+            }
+        }
+
+        public static void CloseLast()
+        {
+            if (canCloseLast || activeWindows.Count > 1)
+                Close(activeWindows.Keys.ElementAt(activeWindows.Count - 1));
+        }
+    }
+}
