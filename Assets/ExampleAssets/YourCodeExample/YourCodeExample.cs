77294377cfb9209a39a27e04c5d54131c6e25c23
Quonce <quonce@mail.ru>
2019-02-13 19:16:01 +0300

    Update example

---
+++ Assets/ExampleAssets/YourCodeExample/YourCodeExample.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using WindowManager;
+
+public class YourCodeExample : MonoBehaviour
+{
+    private void Start()
+    {
+        Window.Init(false);
+        Window.Open("Info Panel");
+    }
+
+    public void OpenURL(string url)
+    {
+        Application.OpenURL(url);
+    }
+}
